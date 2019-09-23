b6a558af07709c71c5998eff868f6f38c8cda708
cjl <2553520536@qq.com>
2019-09-23 17:41:38 +0800

    update 2019-09-23 17:41

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,13 @@ module.exports = ctx => ({
   // 部署站点的基础路径，如果你想让你的网站部署到一个子路径下，你将需要设置它。如 GitHub pages，如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"，它的值应当总是以斜杠开始，并以斜杠结束
   base: '/docs/',
   // 网站的标题，它将会被用作所有页面标题的前缀，同时，默认主题下，它将显示在导航栏（navbar）上
-  title: '项目文档',
+  title: '使用文档',
   // 网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中
-  description: '钉钉小程序（E应用）框架',
+  description: '钉钉小程序框架、氚云开发文档、d2-admin框架',
   // 额外的需要被注入到当前页面的 HTML <head> 中的标签，每个标签都可以以 [tagName, { attrName: attrValue }, innerHTML?] 的格式指定，举个例子，增加一个自定义的 favicon
-  // head: [
-  //   ['link', { rel: 'icon', href: '/logo.png' }]
-  // ],
+  head: [
+    ['link', {rel: 'icon', href: '/logo.jpg'}]
+  ],
   // 指定 dev server 的主机名
   host: '127.0.0.1',
   // 指定 dev server 的端口
@@ -18,14 +18,28 @@ module.exports = ctx => ({
     // 上一次更新时间前缀
     lastUpdated: '上次更新',
     // 源码仓库链接
-    repo: 'cjl255352/e-app',
+    repo: 'cjl255352',
     // 自定义仓库链接文字
-    repoLabel: '查看源码',
+    repoLabel: 'J.L.Chen',
     // 导航栏
     nav: [
-      { text: "指南", link: "/e-app/guide/" },
-      { text: "组件", link: "/e-app/components/" },
-      { text: "页面渲染", link: "/e-app/render/" }
+      {
+        text: "项目",
+        items: [
+          {
+            text: "钉钉小程序框架",
+            link: '/e-app/',
+          },
+          {
+            text: "氚云",
+            link: '/e-app/',
+          },
+          {
+            text: "vue",
+            link: '/e-app/',
+          }
+        ]
+      }
     ],
     // 侧边栏
     sidebar: {
@@ -56,6 +70,7 @@ module.exports = ctx => ({
             'base/user-chooser'
           ]
         },
+        ['/e-app/components/menu/', '菜单组件'],
         {
           title: '其他组件',
           collapsable: false,
--- docs/e-app/guide/README.md
+++ docs/e-app/guide/README.md
@@ -1,10 +1,1 @@
----
-sidebarDepth: 3
-sidebar: auto
----
-
-# 指南
-
-## 介绍
-
-### 目录结构
+# 介绍
